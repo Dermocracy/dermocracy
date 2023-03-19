b15b3629edbc0e9136d21ede72430a5e4a7b2496
Dermocracy <128263966+Dermocracy@users.noreply.github.com>
2023-03-19 07:23:18 +0700

    Update firstinstall.sh

--- firstinstall.sh
+++ firstinstall.sh
@@ -10,6 +10,13 @@ sudo apt-get install -y curl git
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+# Установка PostgreSQL
+sudo apt-get install -y postgresql postgresql-contrib
+
+# Настройка PostgreSQL
+sudo -u postgres createuser --interactive
+sudo -u postgres createdb dermocracy
+
 # Клонирование вашего репозитория
 git clone https://github.com/Dermocracy/dermocracy.git
 cd dermocracy
@@ -17,7 +24,17 @@ cd dermocracy
 # Установка зависимостей проекта
 npm install
 
-# Добавление файла .env с токеном вашего бота
+# Установка knex.js для работы с базой данных
+npm install knex pg
+
+# Инициализация knex.js
+npx knex init
+
+# Добавление файла .env с токеном вашего бота и настройками базы данных
 echo "TELEGRAM_BOT_TOKEN=YOUR_TELEGRAM_BOT_TOKEN" > .env
+echo "DB_HOST=localhost" >> .env
+echo "DB_USER=your_db_user" >> .env
+echo "DB_PASSWORD=your_db_password" >> .env
+echo "DB_NAME=dermocracy" >> .env
 
 echo "Установка завершена. Вы можете запустить вашего бота командой 'node index.js' в папке 'dermocracy'."
