0758176bacddf499d0912f8a643af8ec7ec5c504
Dermocracy <128263966+Dermocracy@users.noreply.github.com>
2023-03-24 18:33:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const { TELEGRAM_BOT_TOKEN } = require("./config");
 const lang = require("./lang");
 const db = require('./db');
-const { pool, getUserByChatId, createUser, updateUser, deleteUser, setLang } = require('./db');
+const { pool, getUserByChatId, createUser, updateUser, deleteUser, setLang, query, getLang } = require('./db');
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 
 bot.on("message", async (msg) => {
