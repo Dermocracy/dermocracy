31d39f06af171762baee002969698befdc2ffe16
Dermocracy <128263966+Dermocracy@users.noreply.github.com>
2023-03-24 10:42:07 +0800

    Update db.js

--- db.js
+++ db.js
@@ -28,3 +28,25 @@ async function startImpeachmentInDB(chatId, endTime) {
 async function updateImpeachmentStatusInDB(chatId, status) {
   // Здесь вы можете добавить логику для обновления статуса импичмента в базе данных.
 }
+// добавьте это в db.js
+async function setLang(chatId, lang) {
+  const query = {
+    text: 'UPDATE users SET lang = $1 WHERE chat_id = $2',
+    values: [lang, chatId],
+  };
+  try {
+    await pool.query(query);
+  } catch (error) {
+    console.error('Error setting language:', error);
+  }
+}
+// добавьте это в конец db.js
+module.exports = {
+  pool,
+  getUsers,
+  getUserByChatId,
+  createUser,
+  updateUser,
+  deleteUser,
+  setLang, // добавьте эту строку
+};
