e4ee500d7d15398ef10e59fd1c865c58c6926ef0
Dermocracy <128263966+Dermocracy@users.noreply.github.com>
2023-03-24 19:16:42 +0800

    Update firstinstall.sh

--- firstinstall.sh
+++ firstinstall.sh
@@ -1,49 +1,72 @@
 #!/bin/bash
 
-# Обновление списка пакетов
-sudo apt-get update
+# Обновление системы и установка зависимостей
+sudo apt update
+sudo apt install -y curl git
 
-# Установка необходимых пакетов
-sudo apt-get install -y curl git
-
-# Установка Node.js и npm
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+# Установка Node.js
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-# Установка PostgreSQL
-sudo apt-get install -y postgresql postgresql-contrib
+# Установка PostgreSQL и его компонентов
+sudo apt install -y postgresql postgresql-contrib
 
-# Настройка PostgreSQL
-sudo -u postgres createuser --interactive
-sudo -u postgres dropdb --if-exists dermocracy
+# Создание пользователя PostgreSQL с паролем
+echo "Enter the new PostgreSQL user:"
+read NEW_PG_USER
+echo "Enter the new PostgreSQL user's password:"
+read NEW_PG_PASSWORD
+sudo -u postgres psql -c "CREATE USER $NEW_PG_USER WITH PASSWORD '$NEW_PG_PASSWORD';"
+
+# Создание базы данных
 sudo -u postgres createdb dermocracy
 
-# Клонирование вашего репозитория
-git clone https://github.com/Dermocracy/dermocracy.git
-cd dermocracy
+# Клонирование репозитория
+git clone https://github.com/username/dermocracy.git
 
 # Установка зависимостей проекта
+cd dermocracy
 npm install
 
-# Установка дополнительных модулей
-npm install node-telegram-bot-api knex pg
+# Создание и обновление файла knexfile.js с учетными данными пользователя базы данных
+cat > knexfile.js << EOF
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: '$NEW_PG_USER',
+      password: '$NEW_PG_PASSWORD',
+      database: 'dermocracy'
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
+};
+EOF
 
-# Инициализация knex.js
-npx knex init
+# Запуск миграций базы данных
+npm run knex -- migrate:latest
 
-# Запрос токена Telegram и учетных данных базы данных
-echo "Введите токен Telegram бота:"
-read telegram_bot_token
-echo "Введите имя пользователя базы данных:"
-read db_user
-echo "Введите пароль пользователя базы данных:"
-read db_password
+# Установка зависимостей для бота
+npm install node-telegram-bot-api
 
-# Добавление файла .env с токеном вашего бота и настройками базы данных
-echo "TELEGRAM_BOT_TOKEN=$telegram_bot_token" > .env
-echo "DB_HOST=localhost" >> .env
-echo "DB_USER=$db_user" >> .env
-echo "DB_PASSWORD=$db_password" >> .env
-echo "DB_NAME=dermocracy" >> .env
+# Запрос токена бота для Telegram
+echo "Enter your bot token for Telegram:"
+read BOT_TOKEN
+
+# Создание .env файла с токеном бота и учетными данными базы данных
+cat > .env << EOF
+TELEGRAM_TOKEN=$BOT_TOKEN
+DB_HOST=localhost
+DB_USER=$NEW_PG_USER
+DB_PASSWORD=$NEW_PG_PASSWORD
+DB_NAME=dermocracy
+EOF
 
 echo "Установка завершена. Вы можете запустить вашего бота командой 'node index.js' в папке 'dermocracy'."
