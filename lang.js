4337af5af71d77945ffdab8e519ad2e2c4208869
Dermocracy <128263966+Dermocracy@users.noreply.github.com>
2023-03-25 03:20:59 +0800

    Update lang.js

--- lang.js
+++ lang.js
@@ -5,6 +5,21 @@ const translations = {
     vote_for_president: 'Vote for president',
     impeach_president: 'Impeach president',
     start_election: 'Start election',
+    no_candidates: "There are currently no candidates to vote for.",
+    vote_for_president: "Vote for a candidate:",
+    vote_successful: "You have successfully voted.",
+    vote_failed: "Failed to vote. Please try again.",
+    candidate_list: "Candidate list:",
+    no_candidates: "There are currently no candidates.",
+    vote_success: "You have successfully voted for the candidate.",
+    vote_error: "An error occurred while voting for the candidate. Please try again.",
+    election_started: "Election has started!",
+    election_start_error: "Election can only be started within 48 hours before the beginning of the next month.",
+    register_candidate: "Register as candidate",
+    view_candidates: "View candidates",
+    vote_for_candidate: "Vote for candidate",
+
+
   },
   ru: {
     greetings: 'Привет, я Dermocracy Bot!',
@@ -12,6 +27,19 @@ const translations = {
     vote_for_president: 'Голосовать за президента',
     impeach_president: 'Импичмент президента',
     start_election: 'Начать выборы',
+    no_candidates: "В настоящее время нет кандидатов для голосования.",
+    vote_for_president: "Голосуйте за кандидата:",
+    vote_successful: "Вы успешно проголосовали.",
+    vote_failed: "Проголосовать не получилось, попробуйте снова.",
+    candidate_list: "Список кандидатов:",
+    no_candidates: "В настоящее время кандидатов нет.",
+    vote_success: "Вы успешно проголосовали за кандидата.",
+    vote_error: "При голосовании за кандидата произошла ошибка. Пожалуйста, попробуйте еще раз.",  
+    election_started: "Выборы начались!",
+    election_start_error: "Выборы могут быть начаты только за 48 часов до начала следующего месяца.",  
+    register_candidate: "Зарегистрироваться как кандидат",
+    view_candidates: "Посмотреть кандидатов",
+    vote_for_candidate: "Проголосовать за кандидата",
   }
 };
 
